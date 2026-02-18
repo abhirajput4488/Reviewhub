bfad1ec9a6e26b05d8be92d732bc49f85850b480
abhirajpoot <abhirao274408@gmail.com>
2026-02-18 16:07:01 +0530

    Implement authentication module with JWT, bcrypt hashing, and protected middleware

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
-
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("API Running...");
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API Running...");
 });
 
-connectDB();
-app.listen(5000,()=>console.log("Server running on 5000"));
+app.listen(5000, () => console.log("Server running on port 5000"));
